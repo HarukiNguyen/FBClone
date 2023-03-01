ed9c9cb873265fcc20f5459a0d4d5f5e2e5006a3
HarukiNguyen <98737510+HarukiNguyen@users.noreply.github.com>
2023-03-01 10:01:50 +0700

    perf: `LoginForm.jsx` > optimize

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import { createContext, useCallback, useState } from 'react';
+import React, { createContext, useCallback, useReducer } from 'react';
 import Input from '../Input/Input';
 import SignupBtn from '../SignupBtn';
 import VerDivider from '../VerDivider';
@@ -6,17 +6,45 @@ import LoginBtn from './LoginBtn';
 
 export const IsValidInfoContext = createContext({});
 
+const initialState = {
+  email: '',
+  pass: '',
+  isValidInfo: {},
+};
+
+const SET_EMAIL = 'setEmail';
+const SET_PASS = 'setPass';
+const SET_IS_VALID_INFO = 'setIsValidInfo';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_EMAIL: {
+      return { ...state, email: action.payload };
+    }
+    case SET_PASS: {
+      return { ...state, pass: action.payload };
+    }
+    case SET_IS_VALID_INFO: {
+      return { ...state, isValidInfo: action.payload };
+    }
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [pass, setPass] = useState('');
-  const [isValidInfo, setIsValidInfo] = useState({});
+  const [{ email, pass, isValidInfo }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const handleLogin = useCallback(
     // TODO(fix): sometimes this function doesn't work when changing values of the inputs
     // Eg: both have valid inputs --> delete email --> can't run this func when click the login button
     (e) => {
       e.preventDefault();
-      if (Object.values(isValidInfo).every((info) => info)) {
+      if (Object.values(isValidInfo).every(Boolean)) {
         // handle login logic
         console.log('valid');
       } else {
@@ -25,6 +53,13 @@ function LoginForm() {
       }
     },
     [isValidInfo]
+  );
+
+  const actionCreator = useCallback(
+    (type, val) => {
+      dispatch({ type, payload: val });
+    },
+    [dispatch]
   );
 
   return (
@@ -39,18 +74,22 @@ function LoginForm() {
           type="email"
           placeholder="Nhập email của bạn"
           value={email}
-          setValue={setEmail}
+          setValue={(value) => actionCreator(SET_EMAIL, value)}
           isEmail
           isRequired
-          setIsValidInfo={setIsValidInfo}
+          setIsValidInfo={(value) => {
+            actionCreator(SET_IS_VALID_INFO, value);
+          }}
         />
         <Input
           type="password"
           placeholder="Mật khẩu"
           value={pass}
-          setValue={setPass}
+          setValue={(value) => actionCreator(SET_PASS, value)}
           isRequired
-          setIsValidInfo={setIsValidInfo}
+          setIsValidInfo={(value) => {
+            actionCreator(SET_IS_VALID_INFO, value);
+          }}
         />
       </IsValidInfoContext.Provider>
       <LoginBtn onClick={handleLogin} />
@@ -68,4 +107,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default React.memo(LoginForm);
